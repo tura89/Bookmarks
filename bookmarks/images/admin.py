b4331147b2c25d55125adf771510c22c706cb6e0
alex <turdzeladze@gmail.com>
2023-04-18 17:01:47 +0400

    bookmarking an image functionality

---
+++ bookmarks/images/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Image
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'image', 'created_at']
+    list_filter = ['created_at']
